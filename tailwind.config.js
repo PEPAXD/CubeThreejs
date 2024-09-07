ed9c6be819f1a9e53d80f9d47f9706438a70efb2
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-09-07 16:51:02 -0300

    addicons socialNetworks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const { addDynamicIconSelectors } = require("@iconify/tailwind");
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [addDynamicIconSelectors()],
+};
